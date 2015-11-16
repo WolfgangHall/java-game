72591774badbaf93c775cdabf107c4835da95475
Johall <johnalexander.w.hall@gmail.com>
2015-11-15 21:35:50 -0500

    changed poke-pic placement and removed clicks.js from home page

--- clicky-game-clicks.js
+++ clicky-game-clicks.js
@@ -1,1 +1,11 @@
+function incrementIndex() {
+ index += 1;
+} 
 
+var addEvent = window.addEventListener ? function (elem, type, method) {
+ elem.addEventListener(type, method, false);
+} : function (elem, type, method) {
+ elem.attachEvent('on' + type, method);
+};
+
+addEvent(poke-pic[0], 'click', incrementIndex);
